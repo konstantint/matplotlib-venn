f38f2715f4531fb34162e36275c1857a5b06bc84
Johannes Buchner <johannes.buchner.acad@gmx.com>
2013-11-22 18:17:18 +0100

    removed unnecessary code

--- matplotlib_venn/_vennnested.py
+++ matplotlib_venn/_vennnested.py
@@ -16,55 +16,7 @@ import matplotlib.transforms as mtransforms
 from matplotlib.patches import FancyBboxPatch
 import itertools
 
-def nestedsets_rectangles(setsizes, labels = None, attrs = None):
-	# assume nested in order
-	nsets = len(setsizes)
-	if labels is None:
-		labels = list(string.ascii_uppercase)[:nsets]
-	if attrs is None:
-		attrs = [dict(ec=c, fc='None', alpha=0.7)
-			for c in ['black', 'blue', 'red', 'green', 'magenta']]
-	((xmin, xmax), (ymin, ymax)) = [(0, 1), (0, 1)]
-	#bb = mtransforms.Bbox([[xmin, ymin], [xmax, ymax]])
-	superset = None
-	ax = plt.gca()
-	ax.set_aspect(1.)
-	ax.set_xlim(-0.1, 1.1)
-	ax.set_ylim(-0.1, 1.1)
-	ax.set_xticks([])
-	ax.set_yticks([])
-	plt.axis('off')
-	for i, (s, l, a) in enumerate(zip(setsizes, labels, attrs)):
-		if i == 0:
-			superset = s
-		else:
-			# update borders, proportional to area
-			ratio = s * 1. / superset
-			f = (ratio * 8/3.)**0.5
-			fx = 1/2. * f
-			fy = 3/4. * f
-			f = ratio**0.5
-			deltay = ymax - ymin
-			deltax = ymax - ymin
-			ymax = ymax - deltay * (1 - fx*0.95)
-			xmin = xmin + deltax * (1 - fy*0.95)
-			ymin = ymin + deltay * (fx*0.05)
-			xmax = xmax - deltax * (fy*0.05)
-		p_fancy = FancyBboxPatch((xmin, ymin),
-			xmax - xmin, ymax - ymin,
-			boxstyle="round,pad=0.02, rounding_size=0.05",
-			**a)
-		ax.add_patch(p_fancy)
-		ax.text(xmin, ymax, l, 
-			horizontalalignment='left',
-			verticalalignment='top',
-			)
-		#draw_bbox(ax, bb)
-		#p_fancy.set_boxstyle("round,pad=0.01, rounding_size=0.05")
-
-#import random
 default_attrs = [dict(ec=c, fc='None', alpha=0.7) for c in ['black', 'blue', 'red', 'green', 'magenta']]
-#random.shuffle(default_attrs)
 default_attrs = itertools.cycle(default_attrs)
 
 def plot_box(bbox, label='', attrs={}):
